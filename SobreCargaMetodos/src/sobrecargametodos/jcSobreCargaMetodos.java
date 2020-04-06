c91e13bad2d953f4b65f87f2167da33377f9d167
Santiago <santiago28oc@hotmail.com>
2020-04-06 15:20:12 -0500

    Se añade version final de Sobre Carga de Metodos

---
+++ SobreCargaMetodos/src/sobrecargametodos/jcSobreCargaMetodos.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sobrecargametodos;
+
+/**
+ *
+ * @author aulab202
+ */
+public class jcSobreCargaMetodos {
+    public int Suma(int N1)
+    {
+        return N1+N1;
+    }
+    public int Suma(int N1, int N2)
+    {
+        return N1+N2;
+    }
+     public int Suma(int N1, int N2, int N3)
+    {
+        return N1+N2+N3;
+    }
+      public double Suma(double N1, int N2, int N3)
+    {
+        return N1+N2+N3;
+    }
+    public int Multiplicacion(int N1)
+    {
+        return N1*N1;
+    }
+    public int Multiplicacion(int N1, int N2)
+    {
+        return N1*N2;
+    }
+     public int Multiplicacion(int N1, int N2, int N3)
+    {
+        return N1*N2*N3;
+    }
+      public double Multiplicacion(double N1, int N2, int N3)
+    {
+        return N1*N2*N3;
+    }
+      
+}
